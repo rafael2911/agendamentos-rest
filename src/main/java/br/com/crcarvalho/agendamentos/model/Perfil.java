e8bbbff4fe8ac21a319427469dafd045e6c6af5e
Rafael Carvalho <crcarvalho@outlook.com>
2019-07-29 12:36:57 -0300

    Liberando acesso e autenticando os usuários

--- src/main/java/br/com/crcarvalho/agendamentos/model/Perfil.java
+++ src/main/java/br/com/crcarvalho/agendamentos/model/Perfil.java
@@ -1,7 +1,18 @@
 package br.com.crcarvalho.agendamentos.model;
 
-public class Perfil {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+public class Perfil implements GrantedAuthority {
 	
+	private static final long serialVersionUID = 1L;
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 
@@ -15,6 +26,12 @@ public class Perfil {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	@Override
+	public String getAuthority() {
+		
+		return this.nome;
 	}
 	
 }
--- src/main/java/br/com/crcarvalho/agendamentos/repository/UsuarioRepository.java
+++ src/main/java/br/com/crcarvalho/agendamentos/repository/UsuarioRepository.java
@@ -1,9 +1,13 @@
 package br.com.crcarvalho.agendamentos.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.crcarvalho.agendamentos.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
+	
+	Optional<Usuario> findByEmail(String email);
+	
 }
