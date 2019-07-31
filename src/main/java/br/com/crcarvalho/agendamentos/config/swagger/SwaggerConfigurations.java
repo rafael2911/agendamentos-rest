7c58cb931ea7da79fb466bb6e8b4b13d3231f542
Rafael Carvalho <crcarvalho@outlook.com>
2019-07-31 12:58:21 -0300

    Enviando token JWT no Swagger

--- src/main/java/br/com/crcarvalho/agendamentos/config/swagger/SwaggerConfigurations.java
+++ src/main/java/br/com/crcarvalho/agendamentos/config/swagger/SwaggerConfigurations.java
@@ -1,23 +1,37 @@
 package br.com.crcarvalho.agendamentos.config.swagger;
 
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import br.com.crcarvalho.agendamentos.model.Usuario;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfigurations {
 	
+	@Bean
 	public Docket agendamentosAPI() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("br.com.crcarvalho.agendamentos"))
 				.paths(PathSelectors.ant("/**"))
 				.build()
-				.ignoredParameterTypes(Usuario.class);
+				.ignoredParameterTypes(Usuario.class)
+				.globalOperationParameters(Arrays.asList(
+						new ParameterBuilder()
+						.name("Authorization")
+						.description("Header para token JWT")
+						.modelRef(new ModelRef("string"))
+						.parameterType("header")
+						.required(false)
+						.build()));
 	}
 	
 }
